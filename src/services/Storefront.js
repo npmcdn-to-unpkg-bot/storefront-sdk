cd83d194b225db785c59dc54ad467b76d5f69235
Breno Calazans <breno@vtex.com.br>
2015-12-14 17:30:23 -0200

    Remove class from Storefront service

--- src/services/Storefront.js
+++ src/services/Storefront.js
@@ -1,47 +1,47 @@
 import axios from 'axios';
 
-class Storefront {
-  constructor() {
-    let token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
-    let workspace = ('; ' + document.cookie).split('; vtex_workspace=').pop().split(';').shift();
+const token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
+const workspace = ('; ' + document.cookie).split('; vtex_workspace=').pop().split(';').shift();
 
-    this.defaultHeaders = {
-      'Authorization': `token ${token}`,
-      'x-vtex-workspace': workspace ? workspace : 'master'
-    };
-  }
+export const defaultHeaders = {
+  'Authorization': `token ${token}`,
+  'x-vtex-workspace': workspace ? workspace : 'master'
+};
 
-  saveAreaSettings({id, component, settings}) {
-    const url = `/_resources/_settings/${id}`;
-    const data = { component, settings };
+export function saveAreaSettings({id, component, settings}) {
+  const url = `/_resources/_settings/${id}`;
+  const data = { component, settings };
 
-    return axios.put(url, data, {
-      headers: this.defaultHeaders
-    });
-  }
-
-  getAreaResources({id, params, query}) {
-    let reqParams = {};
-
-    for (let key in query) {
-      reqParams[`query.${key}`] = query[key];
-    }
-
-    for (let key in params) {
-      reqParams[`route.${key}`] = params[key];
-    }
-
-    return axios.get(`/_areas/${id}/_resources/`, {
-      headers: this.defaultHeaders,
-      params: reqParams
-    });
-  }
-
-  getAreaSettings({id}) {
-    return axios.get(`/_resources/_settings/${id}`, {
-      headers: this.defaultHeaders
-    });
-  }
+  return axios.put(url, data, {
+    headers: defaultHeaders
+  });
 }
 
-export default Storefront;
+export function getAreaResources({id, params, query}) {
+  let reqParams = {};
+
+  for (let key in query) {
+    reqParams[`query.${key}`] = query[key];
+  }
+
+  for (let key in params) {
+    reqParams[`route.${key}`] = params[key];
+  }
+
+  return axios.get(`/_areas/${id}/_resources/`, {
+    headers: defaultHeaders,
+    params: reqParams
+  });
+}
+
+export function getAreaAssets({id}) {
+  return axios.get(`/_areas/${id}/_assets/`, {
+    headers: defaultHeaders
+  });
+}
+
+export function getAreaSettings({id}) {
+  return axios.get(`/_resources/_settings/${id}`, {
+    headers: defaultHeaders
+  });
+}
