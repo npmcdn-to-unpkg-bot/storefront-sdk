03484eb586e090ca9645ee8b736667fce8fe5028
Thor Amorim <thoramorim@gmail.com>
2016-04-07 00:58:19 -0300

    Add check to only put Alt debug on dev mode

--- src/dispatcher/StorefrontDispatcher.js
+++ src/dispatcher/StorefrontDispatcher.js
@@ -1,6 +1,9 @@
 import Alt from 'alt';
 
 let dispatcher = new Alt();
-Alt.debug('alt', dispatcher);
+
+if (process.env.NODE_ENV !== 'production') {
+  Alt.debug('alt', dispatcher);
+}
 
 export default dispatcher;
