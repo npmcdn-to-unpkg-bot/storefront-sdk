ee5779e3e76e4cddc9d62bdcb1dfcfc77bc17382
Thor Amorim <thoramorim@gmail.com>
2016-04-08 15:17:38 -0300

    Remove react-tap-event-plugin

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ import Link from './components/Link';
 import Placeholder from './components/Placeholder';
 import contextify from './utils/contextify';
 
-// Needed for onTouchTap
-// Can go away when react 1.0 release
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
 // Register all actions and stores
 map(actions, (action) => sdk.dispatcher.addActions(action.name, action.obj));
 map(stores, (store) => sdk.dispatcher.addStore(store.name, store.obj, sdk.dispatcher));
